4df7ab7ecb90c9f293340b35906afd9b991bdda3
00MRX00 <17515560@mail.ru>
2020-02-15 09:24:57 +0300

    main page was added
    index.php and scripts/script.js

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Studying</title>
+</head>
+<body>
+	<h1>trenning JavaScript</h1>
+
+	<script src="scripts/script.js"></script>
+</body>
+</html>
